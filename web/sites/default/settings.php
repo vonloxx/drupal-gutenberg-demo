e2656968f072c1600a72c0ac43263166ff1a83ea
Marco Fernandes <marco@front.no>
2019-11-05 17:25:29 +0000

    Add ddev settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -36,3 +36,8 @@ if (file_exists($local_settings)) {
  * modifying settings.php.
  */
 $settings['install_profile'] = 'standard';
+
+// Automatically generated include for settings managed by ddev.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php')) {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
